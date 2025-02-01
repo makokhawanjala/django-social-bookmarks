18075b422d33e75ed12d3dabbc21574b2f7954de
makokhawanjala <amosmakokha084@gmail.com>
2025-02-01 09:30:00 +0300

    Create actions app for activity tracking

---
+++ bookmarks/actions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Action(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='actions', on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    #target = models.CharField(max_length=255, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(ContentType, blank=True, null=True, related_name='target_obj', on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(blank=True, null=True)
+    target = GenericForeignKey('target_ct', 'target_id')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        models.Index(fields=['target_ct', 'target_id']),
+        ordering = ['-created']
